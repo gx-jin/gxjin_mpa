d2b293b37debff7f47044fb8b14689343d3afc1e
gxjin <schach@qq.com>
2024-09-12 20:19:42 +0200

    2024Sep12

--- py-mpa/download_manga_cube.py
+++ py-mpa/download_manga_cube.py
@@ -17,27 +17,43 @@
     In particular, redistribution of the code is not allowed.
 """
 
+
 import os
 import requests
 import shutil
 from astropy.io import fits
-import tqdm
+from tqdm import tqdm
 
 
-def download_logcube(dapall='?', save_dir='?', daptype='SPX', ):
+def download_cube(dapall='?', save_dir='?', daptype='SPX', test=False):
     if not os.path.exists(dapall):
         raise RuntimeError("No such directory: " + str(dapall))
     elif not os.path.exists(save_dir):
         raise RuntimeError("No such directory: " + str(save_dir))
+    elif test:
+        hdu = fits.open(dapall)
+        plate = hdu[1].data['PLATE']
+        ifu = hdu[1].data['IFUDESIGN']
+        i = 3 
+        save_loc = f'{save_dir}/manga-{plate[i]}-{ifu[i]}-MAPS-{daptype}-MILESHC-MASTARSSP.fits.gz'
+        if not os.path.exists(save_loc):
+            maps_url = f'https://data.sdss.org/sas/dr17/manga/spectro/analysis/v3_1_1/3.1.0/{daptype}-MILESHC-MASTARSSP/{plate[i]}/{ifu[i]}/manga-{plate[i]}-{ifu[i]}-LOGCUBE-{daptype}-MILESHC-MASTARSSP.fits.gz'
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; \
+                rv:80.0) Gecko/20100101 Firefox/80.0'}
+            with requests.get(maps_url, headers=headers, stream=True) as r:
+                if r.status_code == requests.codes.ok:
+                    with open(save_loc, 'wb') as f:
+                        shutil.copyfileobj(r.raw, f)
     else:
         hdu = fits.open(dapall)
         plate = hdu[1].data['PLATE']
         ifu = hdu[1].data['IFUDESIGN']
+        dapdone = hdu[1].data['DAPDONE']
         for i in tqdm(range(len(plate))):  
-            save_loc = f'{save_dir}/manga-{plate[i]}-{ifu[i]}-LOGCUBE-{daptype}-MILESHC-MASTARSSP.fits.gz'
+            save_loc = f'{save_dir}/manga-{plate[i]}-{ifu[i]}-MAPS-{daptype}-MILESHC-MASTARSSP.fits.gz'
             if os.path.exists(save_loc):
                 continue
-            else:
+            elif dapdone[i]:
                 maps_url = f'https://data.sdss.org/sas/dr17/manga/spectro/analysis/v3_1_1/3.1.0/{daptype}-MILESHC-MASTARSSP/{plate[i]}/{ifu[i]}/manga-{plate[i]}-{ifu[i]}-LOGCUBE-{daptype}-MILESHC-MASTARSSP.fits.gz'
                 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; \
                     rv:80.0) Gecko/20100101 Firefox/80.0'}
@@ -47,8 +63,9 @@ def download_logcube(dapall='?', save_dir='?', daptype='SPX', ):
                             shutil.copyfileobj(r.raw, f)
 
 
-dapall_dir = './'
-save_dir = './'
-download_logcube(dapall=dapall_dir, save_dir=save_dir, daptype='SPX')
+dapall_dir = '/afs/mpa/home/gxjin/gxjin_mpa/dapall-v3_1_1-3.1.0_spx.fits'
+save_dir = '/afs/mpa/temp/gxjin/DAP11_LOGCUBE_SPX'
+download_cube(dapall=dapall_dir, save_dir=save_dir, daptype='SPX', test=False)
 
 # todo: check how many files, etc.
+
